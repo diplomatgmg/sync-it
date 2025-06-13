9887cefaa36d0453ab7d46b9175d54a064bedd13
Kirill Popov <diplomatgmg@gmail.com>
2025-06-13 18:05:28 +0300

    refactor: structure changed

--- services/parser-service/src/main.py
+++ services/parser-service/src/main.py
@@ -1,16 +1,18 @@
-import logging
 import time
 
-from core.config import parser_config
 
 
-logger = logging.getLogger(__name__)
+def main():
+    print("Hello from parser-service!")
 
+    try:
+        from logger.setup import setup_logging
 
-def main() -> None:
-    logger.info(parser_config.model_config)
-    print(parser_config.model_config)
-    time.sleep(10000)
+        setup_logging()
+    except ModuleNotFoundError:
+        print("logger.setup not found")
+
+    time.sleep(60*60)
 
 
 if __name__ == "__main__":
