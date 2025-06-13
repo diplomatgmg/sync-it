8405ca56ff17d7f8963c9fc0dbe3fcb38ca91d99
Kirill Popov <diplomatgmg@gmail.com>
2025-06-13 12:39:14 +0300

    fixes

--- services/parser-service/src/main.py
+++ services/parser-service/src/main.py
@@ -1,7 +1,15 @@
+import logging
 import time
+
+from core.config import parser_config
+
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
+    logger.info(parser_config.model_config)
+    print(parser_config.model_config)
     time.sleep(10000)
 
 
