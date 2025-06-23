5f1c50b0bb72d7993d8513012638a1b9e60c2046
Kirill Popov <diplomatgmg@gmail.com>
2025-06-23 19:14:35 +0300

    fix: volumes service root

---
+++ services/vacancy-processor/src/database/migrations/versions/2025-06-23_init.py
@@ -0,0 +1,27 @@
+"""init
+
+Revision ID: d753351ee82b
+Revises: 
+Create Date: 2025-06-23 16:13:17.178389
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd753351ee82b'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+
+def upgrade() -> None:
+    op.execute("CREATE SCHEMA IF NOT EXISTS vacancy_parser")  # added manually
+
+
+def downgrade() -> None:
+    op.execute("DROP SCHEMA IF EXISTS vacancy_parser CASCADE")  # added manually
