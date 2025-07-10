0549527957b589e554e30d610bd6c22af833a410
Kirill Popov <diplomatgmg@gmail.com>
2025-07-10 15:45:40 +0300

    fix: deps
    Took 26 minutes
    
    Took 4 seconds
    
    Took 2 minutes

--- services/telegram-bot/src/handlers/preferences.py
+++ services/telegram-bot/src/handlers/preferences.py
@@ -1,23 +1,26 @@
 from aiogram import F, Router
+from aiogram.types import CallbackQuery
+
 from callbacks.preferences import PreferencesActionEnum, PreferencesCallback
 
 
 __all__ = ["router"]
 
+from utils.message import safe_edit_message
 
 router = Router(name=PreferencesCallback.__prefix__)
 
 
 @router.callback_query(PreferencesCallback.filter(F.action == PreferencesActionEnum.WORK_FORMAT))
-async def handle_work_format() -> None:
-    pass
+async def handle_work_format(query: CallbackQuery) -> None:
+    await safe_edit_message(query, text="work_format")
 
 
 @router.callback_query(PreferencesCallback.filter(F.action == PreferencesActionEnum.GRADE))
-async def handle_grade() -> None:
-    pass
+async def handle_grade(query: CallbackQuery) -> None:
+    await safe_edit_message(query, text="grade")
 
 
 @router.callback_query(PreferencesCallback.filter(F.action == PreferencesActionEnum.PROFESSION))
-async def handle_profession() -> None:
-    pass
+async def handle_profession(query: CallbackQuery) -> None:
+    await safe_edit_message(query, text="profession")
