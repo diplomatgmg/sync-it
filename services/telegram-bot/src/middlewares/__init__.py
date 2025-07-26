65752e34b0c78cea55d16a086d21451498d08b9c
Kirill Popov <diplomatgmg@gmail.com>
2025-07-26 11:29:07 +0300

    feat: added logging middleware
    Took 18 minutes

--- services/telegram-bot/src/middlewares/__init__.py
+++ services/telegram-bot/src/middlewares/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
+from common.environment.config import env_config
 from common.logger import get_logger
 from middlewares.auth import AuthMiddleware
 from middlewares.database import DatabaseMiddleware
+from middlewares.logging import LoggingMiddleware
 
 
 __all__ = ["register_middlewares"]
@@ -13,5 +15,9 @@ logger = get_logger(__name__)
 def register_middlewares(dp: Dispatcher) -> None:
     logger.info("Registering middlewares")
 
+    if env_config.debug:
+        dp.update.middleware(LoggingMiddleware())
+
     dp.update.outer_middleware(DatabaseMiddleware())
     dp.message.middleware(AuthMiddleware())
+    dp.callback_query.middleware(AuthMiddleware())
---
+++ services/telegram-bot/src/middlewares/logging.py
@@ -0,0 +1,33 @@
+from collections.abc import Awaitable, Callable
+from typing import TYPE_CHECKING, Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from common.logger import get_logger
+
+
+if TYPE_CHECKING:
+    from aiogram.fsm.context import FSMContext
+
+
+__all__ = ["LoggingMiddleware"]
+
+logger = get_logger(__name__)
+
+
+class LoggingMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        logger.debug(event.model_dump_json(indent=4, exclude_none=True, exclude_defaults=True))
+
+        fsm_context: FSMContext | None = data["state"]
+        if fsm_context:
+            current_state = await fsm_context.get_state()
+            if current_state:
+                logger.debug("FSM State: %s", current_state)
+
+        return await handler(event, data)
