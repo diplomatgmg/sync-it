a49b837c39369bbd55fbab636e6eb7f662fe7a26
Kirill Popov <diplomatgmg@gmail.com>
2025-08-06 16:26:23 +0300

    refactor: reduced beat schedules
    Took 8 minutes

--- services/vacancy-parser/src/celery_app/beat.py
+++ services/vacancy-parser/src/celery_app/beat.py
@@ -6,8 +6,8 @@ from celery.schedules import schedule  # type: ignore[import-untyped]
 __all__ = ["beat_schedule"]
 
 beat_schedule = {
-    "parse-vacancies-every-hour": {
+    "parse-vacancies-every-10-minutes": {
         "task": "parse_vacancies",
-        "schedule": schedule(run_every=timedelta(hours=1)),
+        "schedule": schedule(run_every=timedelta(minutes=1)),
     }
 }
--- services/vacancy-processor/src/celery_app/beat.py
+++ services/vacancy-processor/src/celery_app/beat.py
@@ -6,8 +6,8 @@ from celery.schedules import schedule  # type: ignore[import-untyped]
 __all__ = ["beat_schedule"]
 
 beat_schedule = {
-    "process-vacancies-30-minutes": {
+    "process-vacancies-5-minutes": {
         "task": "process_vacancies",
-        "schedule": schedule(run_every=timedelta(minutes=30)),
+        "schedule": schedule(run_every=timedelta(minutes=5)),
     }
 }
