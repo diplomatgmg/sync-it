0ee564f5016a6b40c9f1db9e6a2ca45cdd5f7f73
Kirill Popov <diplomatgmg@gmail.com>
2025-08-15 17:25:13 +0300

    added new enum value
    Took 40 minutes

--- services/vacancy-parser/src/clients/head_hunter/enums.py
+++ services/vacancy-parser/src/clients/head_hunter/enums.py
@@ -8,14 +8,15 @@ __all__ = [
 
 
 class SalaryCurrency(StrEnum):
-    RUR = "RUR"
-    EUR = "EUR"
-    USD = "USD"
-    KZT = "KZT"
-    BYR = "BYR"
-    GEL = "GEL"
-    UZS = "UZS"
-    KGS = "KGS"
+    RUR = "RUR" # российский рубль
+    EUR = "EUR" # евро
+    USD = "USD" # доллар
+    KZT = "KZT" # тенге
+    BYR = "BYR" # белорусский рубль
+    GEL = "GEL" # грузинский лари
+    UZS = "UZS" # узбекский сум
+    KGS = "KGS" # киргизский сом
+    AZN = "AZN" # азербайджанский манат
 
     def humanize(self) -> str:
         match self:
