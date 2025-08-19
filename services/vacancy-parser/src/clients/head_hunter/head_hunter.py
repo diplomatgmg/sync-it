4ac3bf4576697b110c674c109432c9135f94f27f
Kirill Popov <diplomatgmg@gmail.com>
2025-08-19 15:50:46 +0300

    optimise: rewrite parsing to as_completed
    Took 8 minutes

--- services/vacancy-parser/src/clients/head_hunter/head_hunter.py
+++ services/vacancy-parser/src/clients/head_hunter/head_hunter.py
@@ -78,9 +78,10 @@ class _HeadHunterClient(BaseClient):
         total_pages = first_page_data.pages
         if total_pages > 1:
             logger.info("Fetching remaining %s pages...", total_pages - 1)
-            page_tasks = [self._fetch_page(page=p, text_query=text_query) for p in range(1, total_pages)]
-            page_results = await asyncio.gather(*page_tasks)
-            for page_data in page_results:
+
+            tasks = [self._fetch_page(page=p, text_query=text_query) for p in range(1, total_pages)]
+            for coro in asyncio.as_completed(tasks):
+                page_data = await coro
                 all_vacancy_ids.extend(vacancy.id for vacancy in page_data.items)
 
         return all_vacancy_ids
