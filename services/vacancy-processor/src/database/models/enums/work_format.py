4387eeadc4ba7225de39c0fc9596947693bb38b0
Kirill Popov <diplomatgmg@gmail.com>
2025-08-22 21:46:36 +0300

    fix: lint-fix
    Took 7 minutes

--- services/vacancy-processor/src/database/models/enums/work_format.py
+++ services/vacancy-processor/src/database/models/enums/work_format.py
@@ -1,11 +1,14 @@
+# ruff: noqa: E501
 from database.models.enums import BaseAliasEnum
 
 
 __all__ = ["WorkFormatEnum"]
 
 
+# fmt: off
 class WorkFormatEnum(BaseAliasEnum):
     UNKNOWN = "Неизвестно"
     REMOTE = "Удалёнка", ("удалённо", "удаленная работа", "удалённая работа", "удалено", "удаленка", "удаленна", "удалённая")
     HYBRID = "Гибрид"
     OFFICE = "Офис", ("на месте работодателя",)
+# fmt: on
