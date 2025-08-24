d98187f06ddb926b9fdde08e8524c8ad71a66458
Kirill Popov <diplomatgmg@gmail.com>
2025-08-24 19:40:22 +0300

    refactor: increased vacancies parser schedule
    Took 49 seconds

--- services/vacancy-parser/src/celery_app/beat.py
+++ services/vacancy-parser/src/celery_app/beat.py
@@ -6,8 +6,8 @@ from celery.schedules import schedule
 __all__ = ["beat_schedule"]
 
 beat_schedule = {
-    "parse-vacancies-every-5-minutes": {
+    "parse-vacancies-every-30-minutes": {
         "task": "parse_vacancies",
-        "schedule": schedule(run_every=timedelta(minutes=5)),
+        "schedule": schedule(run_every=timedelta(minutes=30)),
     }
 }
